810676e81d06e05e29959c9141d825c2f67bab5a
Vishal V <109803822+vis465@users.noreply.github.com>
2026-03-07 09:20:42 +0530

    Website down

--- src/App.js
+++ src/App.js
@@ -99,7 +99,7 @@ export const ThemeContext = createContext({
 
 function App() {
   const [isDarkMode, setIsDarkMode] = useState(true);
-  const [maintain, setmaintain] = useState(false);
+  const [maintain, setmaintain] = useState(true);
   const toggleTheme = () => {
     setIsDarkMode(!isDarkMode);
   };
